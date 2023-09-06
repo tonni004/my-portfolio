9359f42aebd63c7aaf993be4a181678082169b40
t_ONNI <109612428+tonni004@users.noreply.github.com>
2023-09-06 17:18:03 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 // import 'modern-normalize/modern-normalize.css';
-
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
